47dd6e743de485ea257a82029442516cf29f535a
HM <hieumdd@gmail.com>
2023-10-10 21:18:24 +0700

    feat: Add index.ts

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import 'dotenv/config';
+import { program } from 'commander';
+
+import { logger } from './logging.service';
+
+import * as pipelines from './pipeline/pipeline.const';
+import { runPipeline } from './pipeline/pipeline.service';
+import { RunPipelineBodySchema } from './pipeline/pipeline.request.dto';
+
+['SIGINT', 'SIGTERM'].forEach((signal) => {
+    process.on(signal, () => {
+        logger.info({ action: 'interupt' });
+        process.exit(0);
+    });
+});
+
+program
+    .requiredOption(
+        '-p, --pipeline <name>',
+        `[Required] Pipeline name, allowed values are ${JSON.stringify(Object.keys(pipelines))}`,
+    )
+    .option(
+        '-s, --start <start>',
+        `for resource based pipelines, in YYYY-MM-DD format. If left blank then it will default to config's settings`,
+    )
+    .option(
+        '-e, --end <end>',
+        `for resource based pipelines, in YYYY-MM-DD format. If left blank then it will default to config's settings`,
+    );
+program.parse(process.argv);
+
+(async () => {
+    const opts = program.opts();
+
+    logger.debug({ fn: 'index', opts });
+
+    const { value, error } = RunPipelineBodySchema.validate(opts);
+
+    if (error) {
+        logger.warn({ fn: 'index', error });
+        return;
+    }
+
+    logger.info({ fn: 'index', value, status: 'start' });
+    await runPipeline(pipelines[value.pipeline], value);
+    logger.info({ fn: 'index', value, status: 'done' });
+})();
