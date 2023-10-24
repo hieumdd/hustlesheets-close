b3a0ad64fd291476ee0936067e02bf7eedecbdf8
HM <hieumdd@gmail.com>
2023-10-24 15:40:46 +0700

    feat: Add getIncrementalStream

--- src/close/close.service.ts
+++ src/close/close.service.ts
@@ -33,6 +33,52 @@ export type GetIncrementalStreamConfig = {
     paramsBuilder: (options: GetIncrementalStreamOptions) => object;
 };
 
+export const getIncrementalStream = ({ uri, paramsBuilder }: GetIncrementalStreamConfig) => {
+    return async (options: GetIncrementalStreamOptions) => {
+        let count = 0;
+        const stream = new Readable({ objectMode: true, read: () => {} });
+
+        const limit = 100;
+        const params = Interval.fromDateTimes(
+            DateTime.fromFormat(options.start, DATE_FORMAT),
+            DateTime.fromFormat(options.end, DATE_FORMAT),
+        )
+            .splitBy({ day: 1 })
+            .map((date) => ({
+                start: (<DateTime>date.start).toFormat(DATE_FORMAT),
+                end: (<DateTime>date.end).toFormat(DATE_FORMAT),
+            }))
+            .map(paramsBuilder);
+
+        pMap(
+            params,
+            async (params) => {
+                const get = async (skip = 0): Promise<any> => {
+                    return client
+                        .request<{ data: object[]; has_more: boolean }>({
+                            method: 'GET',
+                            url: uri,
+                            params: { ...params, _limit: limit, _skip: skip },
+                        })
+                        .then(({ data }) => {
+                            count = count + data.data.length;
+                            logger.debug({ fn: 'getIncrementalStream', uri, progress: count });
+                            data.data.forEach((row) => stream.push(row));
+                            return data.has_more ? get(skip + limit) : undefined;
+                        });
+                };
+
+                return get();
+            },
+            { concurrency: 10 },
+        )
+            .then(() => stream.push(null))
+            .catch((error) => stream.emit('error', error));
+
+        return stream;
+    };
+};
+
 export const getResourceStream = ({ uri, paramsBuilder }: GetIncrementalStreamConfig) => {
     return async (options: GetIncrementalStreamOptions) => {
         let count = 0;
