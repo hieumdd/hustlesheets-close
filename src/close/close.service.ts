fc2f4473e1c68c3ad0c6208c23ce3c256d382667
HM <hieumdd@gmail.com>
2023-10-24 15:45:15 +0700

    refactor: Remove getResourceStream & getActivityStream

--- src/close/close.service.ts
+++ src/close/close.service.ts
@@ -2,13 +2,11 @@ import { Readable } from 'node:stream';
 import axios from 'axios';
 import qs from 'query-string';
 import pMap from 'p-map';
-import { range } from 'lodash';
 import { DateTime, Interval } from 'luxon';
 
+import { DATE_FORMAT } from '../luxon.utils';
 import { logger } from '../logging.service';
 import { getConfig } from '../config.service';
-
-const DATE_FORMAT = 'yyyy-MM-dd';
 
 const client = axios.create({
     baseURL: 'https://api.close.com/api/v1',
@@ -16,15 +14,9 @@ const client = axios.create({
     paramsSerializer: { serialize: (params) => qs.stringify(params, { arrayFormat: 'comma' }) },
 });
 
+type GetResponse = { data: object[]; has_more: boolean };
+
 export type GetExtractStream = (options: GetIncrementalStreamOptions) => Promise<Readable>;
-
-type GetResourceOptions = { uri: string; params: object };
-
-const getResource = ({ uri, params }: GetResourceOptions) => {
-    type GetResourceResponse = { data: object[]; has_more: boolean; total_results: number };
-
-    return client.request<GetResourceResponse>({ method: 'GET', url: uri, params });
-};
 
 export type GetIncrementalStreamOptions = { start: string; end: string };
 
@@ -55,7 +47,7 @@ export const getIncrementalStream = ({ uri, paramsBuilder }: GetIncrementalStrea
             async (params) => {
                 const get = async (skip = 0): Promise<any> => {
                     return client
-                        .request<{ data: object[]; has_more: boolean }>({
+                        .request<GetResponse>({
                             method: 'GET',
                             url: uri,
                             params: { ...params, _limit: limit, _skip: skip },
@@ -73,85 +65,10 @@ export const getIncrementalStream = ({ uri, paramsBuilder }: GetIncrementalStrea
             { concurrency: 10 },
         )
             .then(() => stream.push(null))
-            .catch((error) => stream.emit('error', error));
-
-        return stream;
-    };
-};
-
-export const getResourceStream = ({ uri, paramsBuilder }: GetIncrementalStreamConfig) => {
-    return async (options: GetIncrementalStreamOptions) => {
-        let count = 0;
-        const stream = new Readable({ objectMode: true, read: () => {} });
-
-        const limit = 200;
-        const params = paramsBuilder(options);
-
-        const skips = await getResource({ uri, params }).then(({ data }) => {
-            return Math.ceil(data.total_results / limit);
-        });
-
-        pMap(
-            range(0, skips * limit, limit),
-            async (skip) => {
-                return getResource({ uri, params: { ...params, _limit: limit, _skip: skip } }).then(
-                    ({ data }) => {
-                        count = count + data.data.length;
-                        logger.debug({
-                            fn: 'getResourceStream',
-                            progress: `${count}/${data.total_results}`,
-                        });
-                        data.data.forEach((row) => stream.push(row));
-                    },
-                );
-            },
-            { concurrency: 10 },
-        )
-            .then(() => stream.push(null))
-            .catch((error) => stream.emit('error', error));
-
-        return stream;
-    };
-};
-
-export const getActivityStream = ({ uri, paramsBuilder }: GetIncrementalStreamConfig) => {
-    return async (options: GetIncrementalStreamOptions) => {
-        let count = 0;
-        const stream = new Readable({ objectMode: true, read: () => {} });
-
-        const limit = 100;
-        const params = Interval.fromDateTimes(
-            DateTime.fromFormat(options.start, DATE_FORMAT),
-            DateTime.fromFormat(options.end, DATE_FORMAT),
-        )
-            .splitBy({ day: 1 })
-            .map((date) => ({
-                start: (<DateTime>date.start).toFormat(DATE_FORMAT),
-                end: (<DateTime>date.end).toFormat(DATE_FORMAT),
-            }))
-            .map(paramsBuilder);
-
-        pMap(
-            params,
-            async (params) => {
-                const get = async (skip = 0): Promise<any> => {
-                    return getResource({
-                        uri,
-                        params: { ...params, _limit: limit, _skip: skip },
-                    }).then(({ data }) => {
-                        count = count + data.data.length;
-                        logger.debug({ fn: 'getActivityStream', progress: count });
-                        data.data.forEach((row) => stream.push(row));
-                        return data.has_more ? get(skip + limit) : undefined;
-                    });
-                };
-
-                return get();
-            },
-            { concurrency: 10 },
-        )
-            .then(() => stream.push(null))
-            .catch((error) => stream.emit('error', error));
+            .catch((error) => {
+                logger.error({ fn: 'getIncrementalStream', error });
+                stream.emit('error', error);
+            });
 
         return stream;
     };
@@ -161,17 +78,19 @@ type GetDimensionStreamConfig = { uri: string };
 
 export const getDimensionStream = ({ uri }: GetDimensionStreamConfig) => {
     return async () => {
-        type GetDimensionResponse = { data: object[] };
-
         const stream = new Readable({ objectMode: true, read: () => {} });
 
         client
-            .request<GetDimensionResponse>({ method: 'GET', url: uri })
+            .request<GetResponse>({ method: 'GET', url: uri })
             .then(({ data }) => {
+                logger.debug({ fn: 'getDimensionStream', uri });
                 data.data.forEach((row) => stream.push(row));
                 stream.push(null);
             })
-            .catch((error) => stream.emit('error', error));
+            .catch((error) => {
+                logger.error({ fn: 'getDimensionStream', error });
+                stream.emit('error', error);
+            });
 
         return stream;
     };
